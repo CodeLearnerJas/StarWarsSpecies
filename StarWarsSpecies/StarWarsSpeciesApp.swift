dd5ae3c2fdc0a0b144aed6ef8497994fc49f5f2f
CodeLearnerJas <yangtianjin2013@gmail.com>
2024-10-08 12:26:55 +1100

    Initial Commit

---
+++ StarWarsSpecies/StarWarsSpeciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsSpeciesApp.swift
+//  StarWarsSpecies
+//
+//  Created by GuitarLearnerJas on 8/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsSpeciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StarWarsSpeciesTests/StarWarsSpeciesTests.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsSpeciesTests.swift
+//  StarWarsSpeciesTests
+//
+//  Created by GuitarLearnerJas on 8/10/2024.
+//
+
+import Testing
+@testable import StarWarsSpecies
+
+struct StarWarsSpeciesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
